cf28521adf1947b6c2bd46ba8aac155b1a0f1e93
Callum <39635013+Zelatrix@users.noreply.github.com>
2019-11-14 18:11:43 +0000

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,29 +1,46 @@
 from rply import LexerGenerator
 
-class Lexer():
-  def __init__(self):
-    self.lexer = LexerGenerator()
-  
-  def add_tokens(self):
-    #Exclamation
-    self.lexer.add("BANG", r"\!")
-    #Left parenthesis
-    self.lexer.add("LEFT_PAR", r"\(")
-    #Right parenthesis
-    self.lexer.add("RIGHT_PAR", r"\)")
-    #Digit
-    self.lexer.add("NUM", r"\d+")
-    #Addition
-    self.lexer.add("PLUS", r"\+")
-    #Subtraction
-    self.lexer.add("MINUS", r"\-")
-    #Multiplication
-    self.lexer.add("STAR", r"\*")
-    #Division
-    self.lexer.add("SLASH", r"\/")
-    #Ignore spaces
-    self.lexer.ignore("\s+")
 
-  def get_lexer(self):
-    self.add_tokens()
-    return self.lexer.build()
+class Lexer:
+    # Initialise the lexer object
+      def __init__(self):
+            self.lexer = LexerGenerator()
+
+    # Add the list of tokens to the lexer
+      def add_tokens(self):
+            # Keywords
+            self.lexer.add("FOR", r"for")
+            self.lexer.add("WHILE", r"while")
+            self.lexer.add("IF", r"if")
+            self.lexer.add("DICT", r"dict")
+            self.lexer.add("ARRAY", r"array")
+            self.lexer.add("BANG", r"\!")
+            self.lexer.add("VAR", r"var")
+
+            # Arithmetic
+            self.lexer.add("NUM", r"\d+")
+            self.lexer.add("PLUS", r"\+")
+            self.lexer.add("MINUS", r"\-")
+            self.lexer.add("STAR", r"\*")
+            self.lexer.add("SLASH", r"\/")
+            self.lexer.add("MOD", r"mod")
+
+            # Two-part tokens
+            self.lexer.ignore("\/\/.+")
+            # self.lexer.ignore("/*([A-Z]|[a-z]|[0-9])+*/")
+
+            # Miscellaneous
+            #self.lexer.add("BANG", r"\!")
+            self.lexer.add("DBL_QUOTE", r"\"")
+            self.lexer.add("SINGLE_QUOTE", r"\'")
+            self.lexer.add("LEFT_PAR", r"\(")
+            self.lexer.add("RIGHT_PAR", r"\)")
+            self.lexer.add("SEMICOLON", r"\;")
+            self.lexer.add("COLON", r"\:")
+            self.lexer.ignore("\s+")
+
+    # Add the tokens to the lexer
+    # Build the lexer
+      def get_lexer(self):
+          self.add_tokens()
+          return self.lexer.build()
