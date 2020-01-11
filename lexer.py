abbb4ff1414724106961f32356bcf2d6a5537be8
Callum <39635013+Zelatrix@users.noreply.github.com>
2020-01-11 17:22:46 +0000

    Added lexing of Boolean types and operators
    Added functions, improved comments, and added comparison operators.

--- lexer.py
+++ lexer.py
@@ -1,46 +1,66 @@
 from rply import LexerGenerator
 
 
-class Lexer:
-    # Initialise the lexer object
-      def __init__(self):
-            self.lexer = LexerGenerator()
+class Lexer():
+    def __init__(self):
+        self.lexer = LexerGenerator()
 
-    # Add the list of tokens to the lexer
-      def add_tokens(self):
-            # Keywords
-            self.lexer.add("FOR", r"for")
-            self.lexer.add("WHILE", r"while")
-            self.lexer.add("IF", r"if")
-            self.lexer.add("DICT", r"dict")
-            self.lexer.add("ARRAY", r"array")
-            self.lexer.add("BANG", r"\!")
-            self.lexer.add("VAR", r"var")
+    def add_tokens(self):
+        # Keywords
+        self.lexer.add("FUNCTION", r"function")
+        self.lexer.add("FOR", r"for")
+        self.lexer.add("UNTIL", r"until")
+        self.lexer.add("WHILE", r"while")
+        self.lexer.add("IF", r"if")
+        self.lexer.add("THEN", r"then")
+        self.lexer.add("ARRAY", r"array")
+        self.lexer.add("PRINT", r"print")
+        self.lexer.add("VAR", r"var")
 
-            # Arithmetic
-            self.lexer.add("NUM", r"\d+")
-            self.lexer.add("PLUS", r"\+")
-            self.lexer.add("MINUS", r"\-")
-            self.lexer.add("STAR", r"\*")
-            self.lexer.add("SLASH", r"\/")
-            self.lexer.add("MOD", r"mod")
+        # Boolean operators
+        self.lexer.add("AND", r"(and)")
+        self.lexer.add("OR", r"(or)")
+        self.lexer.add("NOT", r"(not)")
 
-            # Two-part tokens
-            self.lexer.ignore("\/\/.+")
-            # self.lexer.ignore("/*([A-Z]|[a-z]|[0-9])+*/")
+        # Types
+        self.lexer.add("INT", r"(?<!\.)\d+(?!\.)")
+        self.lexer.add("FLOAT", r"\d+\.\d+")
+        self.lexer.add("BOOL", r"(true|false)")
+        self.lexer.add("STRING", r"\".+\"")
+        self.lexer.add("CHAR", r"[A-Za-z]")
 
-            # Miscellaneous
-            #self.lexer.add("BANG", r"\!")
-            self.lexer.add("DBL_QUOTE", r"\"")
-            self.lexer.add("SINGLE_QUOTE", r"\'")
-            self.lexer.add("LEFT_PAR", r"\(")
-            self.lexer.add("RIGHT_PAR", r"\)")
-            self.lexer.add("SEMICOLON", r"\;")
-            self.lexer.add("COLON", r"\:")
-            self.lexer.ignore("\s+")
+        # Identifiers
+        self.lexer.add("IDENTIFIER", r"[A-Za-z_]([A-Za-z_0-9])*")
+
+        # Comparison
+        self.lexer.add("LESS_EQUAL", r"\<\=")
+        self.lexer.add("GREAT_EQUAL", r"\>\=")
+        self.lexer.add("EQUAL", r"\=\=")
+        self.lexer.add("LESS_THAN", r"\<")
+        self.lexer.add("GREATER_THAN", r"\>")
+
+        # Arithmetic
+        self.lexer.add("PLUS", r"\+")
+        self.lexer.add("MINUS", r"\-")
+        self.lexer.add("STAR", r"\*")
+        self.lexer.add("SLASH", r"\/")
+        self.lexer.add("MOD", r"mod")
+
+        # Tokens to ignore
+        self.lexer.ignore("//.+")
+        self.lexer.ignore("\s+")
+
+        # Miscellaneous
+        self.lexer.add("ASSIGN", r"\:\=")
+        self.lexer.add("DBL_QUOTE", r"\"")
+        self.lexer.add("SINGLE_QUOTE", r"\'")
+        self.lexer.add("LEFT_PAR", r"\(")
+        self.lexer.add("RIGHT_PAR", r"\)")
+        self.lexer.add("SEMICOLON", r"\;")
+        self.lexer.add("COLON", r"\:")
 
     # Add the tokens to the lexer
     # Build the lexer
-      def get_lexer(self):
-          self.add_tokens()
-          return self.lexer.build()
+    def get_lexer(self):
+        self.add_tokens()
+        return self.lexer.build()
